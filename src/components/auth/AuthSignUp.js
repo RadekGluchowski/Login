4f615689463578a8ade889ffc106142a74dc5bc5
Radoslawgluchowski_1@wp.pl <Radoslawgluchowski_1@wp.pl>
2019-06-07 14:51:25 +0200

    btn disable if form not valid

--- src/components/auth/AuthSignUp.js
+++ src/components/auth/AuthSignUp.js
@@ -13,6 +13,7 @@ export default class AuthSignUp extends Component {
             validNickname: false,
             validEmail: false,
             validIp: false,
+            btnDisabled: true
         }
     }
 
@@ -32,15 +33,15 @@ export default class AuthSignUp extends Component {
         switch (fieldName) {
             case 'nickname':
                 validNickname = value.match(/[A-Z][a-zA-Z][^#&<>\"~;$^%{}?]{1,20}$/);
-                fieldValidationErrors.nickname = validNickname ? '' : 'Nickname wrong format';
+                fieldValidationErrors.nickname = validNickname ? true : false;
                 break;
             case 'email':
                 validEmail = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
-                fieldValidationErrors.email = validEmail ? '' : 'Email wrong format';
+                fieldValidationErrors.email = validEmail ? true : false;
                 break;
             case 'ip':
-                validIp = value.match(/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/)
-                fieldValidationErrors.ip = validIp ? '' : 'Ip adress wrong format';
+                validIp = value.match(/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/)
+                fieldValidationErrors.ip = validIp ? true : false;
                 break;
             default:
                 break;
@@ -54,11 +55,25 @@ export default class AuthSignUp extends Component {
     }
 
     validateForm() {
-        this.setState({ formValid: this.state.validEmail && this.state.validNickname && this.state.validIp })
+        this.setState({
+            formValid: this.state.validEmail && this.state.validNickname && this.state.validIp
+        }, this.btnDisabling)
     }
+
+    btnDisabling() {
+        this.setState({
+            btnDisabled: this.state.formErrors.nickname && this.state.formErrors.email && this.state.formErrors.ip === true ? false : true
+        })
+
+        console.log(this.state.formErrors.nickname)
+        console.log(this.state.formErrors.email)
+        console.log(this.state.formErrors.ip)
+    }
+
 
     render() {
 
+        console.log(this.state.btnDisabled)
         return (
             <div className='signup-container'>
                 <div className='login-form' >
@@ -74,7 +89,7 @@ export default class AuthSignUp extends Component {
                                         style={{ borderBottom: this.state.validNickname === null ? "1px solid red" : "1px solid black" }}
                                         placeholder="Nickname example: Cat, Dog"
                                     />
-                                    <p> {this.state.formErrors.nickname} </p>
+                                    <p style={{ display: this.state.formErrors.nickname === false ? "" : "none" }}> Nickname wrong format </p>
                                 </div>
                             </div>
 
@@ -87,7 +102,7 @@ export default class AuthSignUp extends Component {
                                         style={{ borderBottom: this.state.validEmail === null ? "1px solid red" : "1px solid black" }}
                                         placeholder="Email example: Statham@gmail.com"
                                     />
-                                    <p> {this.state.formErrors.email}</p>
+                                    <p style={{ display: this.state.formErrors.email === false ? "" : "none" }} > Email wrong format </p>
                                 </div>
                             </div>
 
@@ -100,12 +115,12 @@ export default class AuthSignUp extends Component {
                                         style={{ borderBottom: this.state.validIp === null ? "1px solid red" : "1px solid black" }}
                                         placeholder="Ip example: 52.12.111.251"
                                     />
-                                    <p> {this.state.formErrors.ip}</p>
+                                    <p style={{ display: this.state.formErrors.ip === false ? "" : "none" }} > Ip adress wrong format </p>
                                 </div>
                             </div>
                         </form>
                     </div>
-                    <div className='btn-add'>  <button type="submit" disabled={!this.state.formValid} > Add user </button> </div>
+                    <div className='btn-add'>  <button disabled={this.state.btnDisabled}  > Add user </button> </div>
                 </div>
             </div>
         )
